76c18a2e3f5ac571ed6805de857e90db14bd7b5b
Rodrigo M <rodrigo@Ubuntu.myguest.virtualbox.org>
2024-05-06 19:01:03 -0600

    Se recorre la primera fila y se guarda el valor del cuadrado maximo

--- ejercicio3.c
+++ ejercicio3.c
@@ -3,11 +3,19 @@
 
 #include <stdlib.h>
 
-
 #define MAX_SIZE 10
 
 
 int findLargestSquare(int matrix[][MAX_SIZE], int size) {
-    int dp[MAX_SIZE][MAX_SIZE] = {0};
+    int am[MAX_SIZE][MAX_SIZE] = {0};
     int maxSquareSize = 0;
+
+    for (int i = 0; i < size; i++) {
+        am[i][0] = matrix[i][0];
+        am[0][i] = matrix[0][i];
+        if (am[i][0] == 1 || am[0][i] == 1) {
+            maxSquareSize = 1;
+        }
+    }
+
 
