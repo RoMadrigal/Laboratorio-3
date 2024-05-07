426e4c967aa4b6fe810e7a54544fcea9184a716b
Rodrigo M <rodrigo@Ubuntu.myguest.virtualbox.org>
2024-05-06 19:20:32 -0600

    Recorre toda la matriz y devuelve el tamano del cuadrado mas grande

--- ejercicio3.c
+++ ejercicio3.c
@@ -18,4 +18,24 @@ int findLargestSquare(int matrix[][MAX_SIZE], int size) {
         }
     }
 
+    for (int i = 1; i < size; i++) {
+        for (int j = 1; j < size; j++) {
+            if (matrix[i][j] == 1) {
+                am[i][j] = 1 + (am[i-1][j] > 0 && am[i][j-1] > 0 && am[i-1][j-1] > 0 ?
+                                (am[i-1][j] < am[i][j-1] ? (am[i-1][j] < am[i-1][j-1] ? am[i-1][j] : am[i-1][j-1]) : (am[i][j-1] < am[i-1][j-1] ? am[i][j-1] : am[i-1][j-1])) : 0);
+                if (am[i][j] > maxSquareSize) {
+                    maxSquareSize = am[i][j];
+                }
+            }
+        }
+    }
+
+
+    return maxSquareSize;
+}
+
+
+
+
+
 
