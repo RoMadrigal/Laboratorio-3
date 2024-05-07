9100db200eee45f448dd9c9b138b3ea1d0179c65
Rodrigo M <rodrigo@Ubuntu.myguest.virtualbox.org>
2024-05-06 18:49:08 -0600

    Agrega encabezado y comienza nueva funcion

--- ejercicio3.c
+++ ejercicio3.c
@@ -1,4 +1,13 @@
 
 #include <stdio.h>
 
+#include <stdlib.h>
+
+
+#define MAX_SIZE 10
+
+
+int findLargestSquare(int matrix[][MAX_SIZE], int size) {
+    int dp[MAX_SIZE][MAX_SIZE] = {0};
+    int maxSquareSize = 0;
 
